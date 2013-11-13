7e544341053b9fe77539b0d77ce258eda0d02199
D4rKn3sSyS <jesdga95@gmail.com>
2013-11-13 18:50:32 +0100

    Lockscreen: See through (1/2)
    Add the ability to see through lockscreen.
    Adapted to kitkat from jellybean by @BigBrother1984
    
    Signed-off-by: D4rKn3sSyS <jesdga95@gmail.com>
    Signed-off-by: Carlo Savignano <stevewatersy@gmail.com>
    
    PS2:
    
    * Adding missing sem-colon.
    
    Signed-off-by: Carlo Savignano <stevewatersy@gmail.com>
    
    Change-Id: I9057a6e302a5a824799844f27e8f64ff677a520a
    
    PS3:
    
    * Replace and remove undefined mContext.
    * Define the boolean enableScreenRotation inside new method.
    * Whitespaces clean up.
    
    Signed-off-by: Carlo Savignano <stevewatersy@gmail.com>
    
    PS4:
    
    * Restore mContext in setKeyguardParams().
    
    Signed-off-by: Carlo Savignano <stevewatersy@gmail.com>
    
    PS5:
    
    * Fixes for setKeyguardParams() and maybeCreateKeyguardLocked() methods, kitkat changed some things.
    
    Signed-off-by: Carlo Savignano <stevewatersy@gmail.com>
    
    PS6:
    
    * Missing sem-colon.
    
    Signed-off-by: Carlo Savignano <stevewatersy@gmail.com>
    
    PS7: Fix all the things.
    
    * Effects doesn't vanish after first unlock.
    * Created condition in updateShowWallpaper() method that just return if see through is activated, he was restoring the wallpaper at second lock.
    
    Signed-off-by: Carlo Savignano <stevewatersy@gmail.com>
    
    PS8: Cosmetic changes.
    
    * Correct java style name to seeThrough() and place it above updateShowWallpaper().
    
    Signed-off-by: Carlo Savignano <stevewatersy@gmail.com>
    
    PS9: Other cosmetic changes.
    
    * Better name to the see through boolean.
    * Removed a space nitpick.
    
    Signed-off-by: Carlo Savignano <stevewatersy@gmail.com>
    
    PS10:
    
    * We had two booleans for the same thing, ditch the one defined in one class only and take the global.
    
    Signed-off-by: Carlo Savignano <stevewatersy@gmail.com>
    
    Change-Id: I06d952f85baf284961a24c8c9f90b29f4aef719a
    
    Conflicts:
    	core/java/android/provider/Settings.java

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -4045,6 +4045,12 @@ public final class Settings {
         public static final String RECENT_PANEL_SHOW_TOPMOST = "recent_panel_show_topmost";
 
         /**
+         * Allows to show the background activity back the lockscreen
+         * @hide
+         */
+        public static final String LOCKSCREEN_SEE_THROUGH = "lockscreen_see_through";
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
