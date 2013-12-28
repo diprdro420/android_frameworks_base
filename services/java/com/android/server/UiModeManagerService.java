45164ea581342d43d34549faec2c5f25a35d2f1f
Lars Greiss <kufikugel@googlemail.com>
2013-12-28 01:57:47 +0100

    fb: TRDS some tweaks and fixes (1/2)
    some smaller issues fixed and optimizations:
    
    - fix twilightmode was wrong assigned to wrong value
    - ligtsensor mode after first immediattely switch it does
      not allow for 5sec to switch again....after the 5sec it switches
      to current state if the sensor changed its state in this time.
      This prevends on the one hand on bad light conditions a permanent
      switch between dark/light as well it does not bring the system into
      troubles due of to much configuration changes at the same time
    - get rid completly of the observe for the UI_THEME_MODE value and
      handle all over the UIModeManager
    - adapt slimActions and settings to this new behaviour/method
    - some cleanup and minor fixes
    
    Change-Id: I4b2401bf7bef992ad3112ae5ccc852cd29b46bd0
    
    Conflicts:
    	services/java/com/android/server/UiModeManagerService.java

--- services/java/com/android/server/UiModeManagerService.java
+++ services/java/com/android/server/UiModeManagerService.java
@@ -93,6 +93,7 @@ final class UiModeManagerService extends IUiModeManager.Stub
     private int mCurUiMode = 0;
     private int mSetUiMode = 0;
     private int mSetUiThemeMode = 0;
+
     private boolean mAllowConfigChange = true;
     private float mCurrentSwitchLevel = DARK_CONDITION;
 
@@ -332,7 +333,7 @@ final class UiModeManagerService extends IUiModeManager.Stub
 
             if (mAllowConfigChange) {
                 mAllowConfigChange = false;
-                mHandler.postDelayed(mReleaseUiThemeModeBlock, 3000);
+                mHandler.postDelayed(mReleaseUiThemeModeBlock, 5000);
                 synchronized (mLock) {
                     if (mSystemReady) {
                         sendConfigurationLocked();
@@ -527,23 +528,10 @@ final class UiModeManagerService extends IUiModeManager.Stub
         if (mSetUiMode != mConfiguration.uiMode
                 || mSetUiThemeMode != mConfiguration.uiThemeMode) {
             mSetUiMode = mConfiguration.uiMode;
-
-            if (mSetUiThemeMode != mConfiguration.uiThemeMode) {
-                final IStatusBarService barService = IStatusBarService.Stub.asInterface(
-                        ServiceManager.getService(Context.STATUS_BAR_SERVICE));
-                try {
-                    if (barService != null) {
-                        barService.collapsePanels();
-                    }
-                } catch (RemoteException e) {
-                    Slog.w(TAG, "Failure communicating with statusbar service", e);
-                }
-
-                mSetUiThemeMode = mConfiguration.uiThemeMode;
-                Settings.Secure.putIntForUser(mContext.getContentResolver(),
-                        Settings.Secure.UI_THEME_MODE, mSetUiThemeMode,
-                        UserHandle.USER_CURRENT);
-            }
+            mSetUiThemeMode = mConfiguration.uiThemeMode;
+            Settings.Secure.putIntForUser(mContext.getContentResolver(),
+                    Settings.Secure.UI_THEME_MODE, mSetUiThemeMode,
+                    UserHandle.USER_CURRENT);
 
             try {
                 ActivityManagerNative.getDefault().updateConfiguration(mConfiguration);
