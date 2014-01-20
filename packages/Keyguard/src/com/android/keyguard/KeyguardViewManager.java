2a11280404a5094d69a8c5662903fa33b2c761d9
David96 <xd_e-mail@gmx.de>
2014-01-20 15:35:25 +0100

    Add "show all notifications" navbar button
    Change-Id: Id591aa98d462b0aa6d6e146d35d6be06608e7ac1

--- packages/Keyguard/src/com/android/keyguard/KeyguardViewManager.java
+++ packages/Keyguard/src/com/android/keyguard/KeyguardViewManager.java
@@ -1049,6 +1049,10 @@ public class KeyguardViewManager {
         }
     }
 
+    public void dispatchButtonClick(int buttonId) {
+        mNotificationView.showAllNotifications();
+    }
+
     public void launchCamera() {
         if (mKeyguardView != null) {
             mKeyguardView.launchCamera();
