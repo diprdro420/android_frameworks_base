c6fdb24a4340c69c838937cf28643521f11e437c
Jubakuba <Jubakuba@gmail.com>
2014-04-13 20:21:17 -0600

    Framework: Shake to Temporarily Secure && Fixes (1/2)
    PS2: Reduce bloat, grab correct user
    	Dependency
    PS3: Rebase correctly
    PS4: Tired of working these commits into eachother
    	correctly, merge into this one commit.
    	Fix simpin && face unlock
    	Disable shake security if device admin
    	requires screen security of any sort
    PS5: Hack to fix double-inflation on boot
    	registers a listener that stays alive
    	until you shake
    PS6: Better logic from introduction in PS5
            && make the initial feature enabled
            boolean check for security enabled
            before processing everything else
            without reason.
    PS7: Fix potential issue with timer
            && shake staying registered.  Still
            retains accuracy of  without
            unnecessary processing due to
            PS6's security check.
    
    Commit changes: Shake to temporarily secure
    	Fix all lock-before-unlock issues
    	Shake to temporarily secure timer
    
    Lock-Before-Unlock issues fixed:
    	On rotate, secure lock is shown without
    		slider.
    	On boot, secure lock is shown without
    		slider
    	Widgets aren't focused correctly if
    		screen is turned on twice
    		without unlocking
    	Unlock target cut-off in landscape
    	Smaller unlock ring size && mismatched
    		bouncer size
    
    Change-Id: Id6b0b85d2c7d086e1bd1199d7206b1e96b345328
    
    Conflicts:
    	packages/Keyguard/src/com/android/keyguard/KeyguardHostView.java

--- packages/Keyguard/src/com/android/keyguard/KeyguardSecurityModel.java
+++ packages/Keyguard/src/com/android/keyguard/KeyguardSecurityModel.java
@@ -78,6 +78,10 @@ public class KeyguardSecurityModel {
     }
 
     SecurityMode getSecurityMode() {
+        return getSecurityMode(false);
+    }
+
+    SecurityMode getSecurityMode(boolean forceShowInsecure) {
         KeyguardUpdateMonitor updateMonitor = KeyguardUpdateMonitor.getInstance(mContext);
         IccCardConstants.State simState = updateMonitor.getSimState();
 
@@ -100,6 +104,10 @@ public class KeyguardSecurityModel {
                 && mLockPatternUtils.isPukUnlockScreenEnable()) {
             mode = SecurityMode.SimPuk;
         } else if (mLockPatternUtils.getActiveProfileLockMode() != Profile.LockMode.INSECURE) {
+            if (forceShowInsecure) {
+                // Slider or sim locks are forced by user
+                return mode;
+            }
             final int security = mLockPatternUtils.getKeyguardStoredPasswordQuality();
             switch (security) {
                 case DevicePolicyManager.PASSWORD_QUALITY_NUMERIC:
