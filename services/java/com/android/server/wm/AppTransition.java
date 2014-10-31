AOKP/frameworks_base

bring animation back from traveling in hyperspace
Change-Id: I920f5e38ccf6bedb1fc242c7adbe9304798268d4
Signed-off-by: Dirk Rettschlag <dirk.rettschlag@gmail.com>

services/java/com/android/server/wm/AppTransition.java
@@ -910,6 +910,6 @@ public class AppTransition implements Dump {
         mNoOverrides = Settings.AOKP.getBoolean(resolver, Settings.AOKP.ANIMATION_CONTROLS_NO_OVERRIDE, false);
         mExitOnly = Settings.AOKP.getBoolean(resolver, Settings.AOKP.ANIMATION_CONTROLS_EXIT_ONLY, true);
         mReverseExit = Settings.AOKP.getBoolean(resolver, Settings.AOKP.ANIMATION_CONTROLS_REVERSE_EXIT, false);
-        mAnimationDuration = Settings.AOKP.getInt(resolver, Settings.AOKP.ANIMATION_CONTROLS_DURATION, 25);
+        mAnimationDuration = 15 * Settings.AOKP.getInt(resolver, Settings.AOKP.ANIMATION_CONTROLS_DURATION, 25);
     }
 }

diprdro420/android_frameworks_base

services/java/com/android/server/wm/AppTransition.java
@@ -910,6 +910,6 @@ public class AppTransition implements Dump {
         mNoOverrides = Settings.System.getBoolean(resolver, Settings.System.ANIMATION_CONTROLS_NO_OVERRIDE, false);
         mExitOnly = Settings.System.getBoolean(resolver, Settings.System.ANIMATION_CONTROLS_EXIT_ONLY, true);
         mReverseExit = Settings.System.getBoolean(resolver, Settings.System.ANIMATION_CONTROLS_REVERSE_EXIT, false);
-        mAnimationDuration = Settings.System.getInt(resolver, Settings.System.ANIMATION_CONTROLS_DURATION, 25);
+        mAnimationDuration = 15 * Settings.System.getInt(resolver, Settings.System.ANIMATION_CONTROLS_DURATION, 25);
     }
 }