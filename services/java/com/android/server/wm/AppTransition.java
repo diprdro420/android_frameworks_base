AOKP/frameworks_base

base: make animations faster by default
*Why multiply the animation duradtion, which is 0, by 15 to get the actual animation
duration which is still 0.
*Just set mAnimationDuration to equal the AOKP.ANIMATION_CONTROLS_DURATION

Much efficency. Wow. Very Zero Product Property.

Change-Id: Ic33d80421285f2901429a0afb6d23e07741d65c2
Signed-off-by: Andrew Warfield <hiasant@gmail.com>

services/java/com/android/server/wm/AppTransition.java
@@ -911,7 +911,6 @@ public class AppTransition implements Dump {
         mExitOnly = Settings.AOKP.getBoolean(resolver, Settings.AOKP.ANIMATION_CONTROLS_EXIT_ONLY, true);
         mReverseExit = Settings.AOKP.getBoolean(resolver, Settings.AOKP.ANIMATION_CONTROLS_REVERSE_EXIT, false);
 
-        int temp = Settings.AOKP.getInt(resolver, Settings.AOKP.ANIMATION_CONTROLS_DURATION, 0);
-        mAnimationDuration = temp * 15;
+        mAnimationDuration = Settings.AOKP.getInt(resolver, Settings.AOKP.ANIMATION_CONTROLS_DURATION, 25);
     }
 }

diprdro420/android_frameworks_base

services/java/com/android/server/wm/AppTransition.java
@@ -910,8 +910,6 @@ public class AppTransition implements Dump {
         mNoOverrides = Settings.System.getBoolean(resolver, Settings.System.ANIMATION_CONTROLS_NO_OVERRIDE, false);
         mExitOnly = Settings.System.getBoolean(resolver, Settings.System.ANIMATION_CONTROLS_EXIT_ONLY, true);
         mReverseExit = Settings.System.getBoolean(resolver, Settings.System.ANIMATION_CONTROLS_REVERSE_EXIT, false);
-
-        int temp = Settings.System.getInt(resolver, Settings.System.ANIMATION_CONTROLS_DURATION, 0);
-        mAnimationDuration = temp * 15;
+        mAnimationDuration = Settings.System.getInt(resolver, Settings.System.ANIMATION_CONTROLS_DURATION, 25);
     }
 }